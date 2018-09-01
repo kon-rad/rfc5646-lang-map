c88ca45fe959df71e10e50c3257dfdfa53b130a1
konradgnat <konradmgnat@gmail.com>
2018-09-01 16:48:00 -0500

    webpack

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@
 export default class LangCode {
 
   static getLanguageName(code) {
-
+    return 'hello world';
   }
 
   static getLanguageNativeName(code) {
+    return 'hello world';
 
   }
 
   static validateLanguageCode(code) {
+    return 'hello world';
 
   }
 }
